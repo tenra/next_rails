48739d93b204d527ec0db35cc8d3b9be76e30b27
tenra <tetenchi@yahoo.co.jp>
2021-11-07 18:06:22 +0900

    api answer_create action

--- back/app/controllers/answers_controller.rb
+++ back/app/controllers/answers_controller.rb
@@ -1,4 +1,17 @@
 class AnswersController < ApplicationController
+
   def create
+    @answer = Answer.new(answer_params)
+
+    if @answer.save
+      render json: @answer, status: :created, location: @answer
+    else
+      render json: @answer.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def answer_params
+    params.require(:answer).permit(:content, :post_id)
   end
 end
